25e55e524465c38170d15269737d59777a481105
Asma Almonajed <asmaalmonajed0.0@gmail.com>
2026-03-18 02:56:00 +0300

    Create E. Area of a Circle.cpp

---
+++ Codeforces/Assiut University/Sheet #1/E. Area of a Circle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    const double pi = 3.141592653;
+    double r;
+
+    cin >> r;
+
+    cout << fixed << setprecision(9) << (r * r * pi);
+
+    return 0;
+}
