35cd8c99c0b058257fe242a24b0561581242fea0
Asma Almonajed <asmaalmonajed0.0@gmail.com>
2026-03-18 02:29:09 +0300

    Create B. Basic Data Types.cpp

---
+++ Codeforces/Assiut University/Sheet #1/B. Basic Data Types.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    long long y;
+    char c;
+    float t;
+    double d;
+
+    cin >> x >> y >> c >> t >> d;
+
+    cout << x << endl;
+    cout << y << endl;
+    cout << c << endl;
+    cout << t << endl;
+    cout << d << endl;
+
+    return 0;
+}
