346f03c184238a8520db79a2b1b50aaa6a8342b7
Asma Almonajed <asmaalmonajed0.0@gmail.com>
2026-03-26 19:59:56 +0300

    Create M. Capital or Small or Digit.cpp

---
+++ Codeforces/Assiut University/Sheet #1/M. Capital or Small or Digit.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char x;
+    cin >> x;
+
+    if (x >= '0' && x <= '9')
+        cout << "IS DIGIT\n";
+    else
+    {
+        cout << "ALPHA" << endl;
+
+        if (x >= 'a' && x <= 'z')
+            cout << "IS SMALL" << endl;
+        else
+            cout << "IS CAPITAL" << endl;
+    }
+
+    return 0;
+}
