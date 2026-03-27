491ce779e37de6fcb768a358d45d4f8cfae7eb7d
Asma Almonajed <asmaalmonajed0.0@gmail.com>
2026-03-27 13:51:15 +0300

    Create N. Char.cpp

---
+++ Codeforces/Assiut University/Sheet #1/N. Char.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char x;
+    cin >> x;
+
+    if(x >= 'a' && x <= 'z')
+    {
+        cout << (char)(x - 32) << endl;
+    }
+    else
+    {
+        cout << (char)(x + 32) << endl;
+    }
+
+    return 0;
+}
